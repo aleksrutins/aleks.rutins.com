c32418459620c29c43d5b40228848dfae69540d4
Aleks Rūtiņš <aleks@rutins.com>
2023-10-22 13:12:36 -0400

    White background

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -7,7 +7,7 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body class="flex flex-row w-screen h-screen bg-stone-50 font-sans" x-data="{ sidebarOpen: false }">
+<body class="flex flex-row w-screen h-screen bg-white font-sans" x-data="{ sidebarOpen: false }">
     <x-sidebar/>
     <div class="flex-grow transition-[margin,filter]" :class="{'pointer-events-none blur': sidebarOpen, 'ml-[84px]': !sidebarOpen}">
         @yield('content')
