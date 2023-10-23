66cacfa1528ebbf9a39079bc47a74a9e0af975d4
Aleks Rūtiņš <aleks@rutins.com>
2023-10-22 21:03:06 -0400

    Use wire:navigate for blog posts

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -6,7 +6,7 @@
     <h1 class="text-6xl font-black pb-10">Blog</h1>
     <div class="flex flex-row flex-wrap">
         @foreach ($posts as $post)
-            <a class="block border shadow-md hover:shadow-lg transition cursor-pointer p-6 rounded-lg max-w-md" href="{{ route('post', $post->slug) }}">
+            <a class="block border shadow-md hover:shadow-lg transition cursor-pointer p-6 rounded-lg max-w-md" href="{{ route('post', $post->slug) }}" wire:navigate.hover>
                 <h2 class="text-2xl font-black">{{ $post->title }}</h2>
                 <p class="py-2">
                     <img class="rounded-full w-4 h-4 inline align-middle" src="{{ Util::getGravatarURL($post->author_email) }}">
