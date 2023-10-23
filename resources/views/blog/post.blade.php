9e93e7a8af0a833f9357adc671834496a14a9fa5
Aleks Rūtiņš <aleks@rutins.com>
2023-10-23 09:59:03 -0400

    Add updated date to post page

--- resources/views/blog/post.blade.php
+++ resources/views/blog/post.blade.php
@@ -6,7 +6,13 @@
     <h1 class="text-6xl font-black pb-2">{{ $post->title }}</h1>
     <p class="pb-10">
         <img class="rounded-full w-4 h-4 inline align-middle" src="{{ Util::getGravatarURL($post->author_email) }}">
-        <span class="align-middle">{{ $post->author_email }} • <time datetime="{{ $post->created_at }}">{{ $post->created_at->toDateString() }}</time></span>
+        <span class="align-middle">
+            {{ $post->author_email }}
+            • <time datetime="{{ $post->created_at }}">{{ $post->created_at->toDateString() }}</time>
+            @if ($post->updated_at && $post->updated_at != $post->created_at)
+            • Last updated on <time datetime="{{ $post->updated_at }}">{{ $post->updated_at->toDateString() }}</time>
+            @endif
+        </span>
     </p>
     <x-markdown class="prose">
         {{ $post->content }}
