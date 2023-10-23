ff17f1f37a7229a0e99c38ce7bc4ce7ce94a4e9e
Aleks Rūtiņš <aleks@rutins.com>
2023-10-23 18:06:46 -0400

    floating sidebar!

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,4 +1,4 @@
-<div class="flex flex-col gap-3 z-10 bg-stone-100/75 backdrop-blur border-r p-5 h-screen fixed transition-all" :class="{'shadow-lg rounded-lg border mt-2 ml-2 h-[calc(100vh-1.3rem)]': sidebarOpen}" x-on:mouseenter="sidebarOpen = true" x-on:mouseleave="sidebarOpen = false" x-data>
+<div class="flex flex-col gap-3 z-10 bg-stone-100/75 backdrop-blur border p-5 h-[calc(100vh-0.5rem)] fixed transition-all shadow-sm rounded-lg mt-1 ml-1" :class="{'shadow-lg mt-2 ml-2 h-[calc(100vh-1.35rem)]': sidebarOpen}" x-on:mouseenter="sidebarOpen = true" x-on:mouseleave="sidebarOpen = false" x-data>
     <a class="cursor-pointer font-black text-lg block text-center self-center" href="{{ route('home') }}">
         A<span x-show="sidebarOpen" x-transition>LEKS</span><span x-show="sidebarOpen"> </span>R<span x-show="sidebarOpen" x-transition>ŪTIŅŠ</span>
     </a>
